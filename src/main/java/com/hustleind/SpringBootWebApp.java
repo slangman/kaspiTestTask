d9bc802e2a7bb83128d39de3c27743411c3158f9
d.ivantsov.stc <d.ivantsov.stc@innopolis.ru>
2018-09-10 20:47:06 +0600

    Initial commit

---
+++ src/main/java/com/hustleind/SpringBootWebApp.java
@@ -0,0 +1,18 @@
+package com.hustleind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootWebApp extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApp.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApp.class, args);
+    }
+}
